4e79cedec2a4b51114bc62b36be949c2a45d1ada
CarlosGEA <carlosgea31@gmail.com>
2025-01-28 12:08:52 +0000

    more attempts on 2d-dp

--- 2D-DP/First/RegEx Matching.py
+++ 2D-DP/First/RegEx Matching.py
@@ -12,7 +12,31 @@ class Solution:
         '.' Matches any single character
         '*' Matches zero or more of the preceding element.
         """
-        return
+
+        memo = {}
+
+        def dfs(i, j):
+
+            if j == len(p):
+                return i == len(s)
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            matches = i < len(s) and (s[i] == p[j] or p[j] == ".")
+
+            if j + 1 < len(p) and p[j + 1] == "*":
+                memo[(i, j)] = dfs(i, j + 2) or (matches and dfs(i + 1, j))
+                return memo[(i, j)]
+
+            if matches:
+                memo[(i, j)] = dfs(i + 1, j + 1)
+                return memo[(i, j)]
+
+            memo[(i, j)] = False
+            return memo[(i, j)]
+
+        return dfs(0, 0)
 
 
 def main():
--- 2D-DP/Intermediate/Edit Distance2.py
+++ 2D-DP/Intermediate/Edit Distance2.py
@@ -6,29 +6,29 @@ Date created : 28-01-2025
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # good, make more efficient the return ad check statments
-        # don't need a return 0
-        # pure if and else within dfs
+
         memo = {}
 
         def dfs(i, j):
 
-            if i >= len(word1):
-                return len(word2) - j
+            if i == len(word1):
+                return len(word2) - j  # just insert
 
-            elif j >= len(word2):
-                return len(word1) - i
+            if j == len(word2):
+                return len(word1) - i  # just remove
+
             if (i, j) in memo:
                 return memo[(i, j)]
 
+            res = 0
             if word1[i] == word2[j]:
-                res = dfs(i + 1, j + 1)
+                res += dfs(i + 1, j + 1)
 
             else:
-                res = 1 + min(dfs(i, j + 1), dfs(i + 1, j), dfs(i + 1, j + 1))
+                res += 1 + min(dfs(i + 1, j), dfs(i, j + 1), dfs(i + 1, j + 1))
+                # delete, insert, replace
 
             memo[(i, j)] = res
-
             return res
 
         return dfs(0, 0)
